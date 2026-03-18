e44b82b850444e04f01904f0a39623ee807ef8cf
kavindulakmal2000 <kavindulakmal2000@gmail.com>
2026-03-19 03:47:29 +0530

    update service and fix link connection

--- src/services/service.js
+++ src/services/service.js
@@ -1,6 +1,6 @@
 export async function getPlans() {
   try {
-    const res = await fetch(`./gym-promotional-site/plans.json`);
+    const res = await fetch(`./plans.json`);
     if (!res.ok) throw new Error("Failed to load plans");
     const data = await res.json();
     return data;
