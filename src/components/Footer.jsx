6a9fefc45523b1fbe47959006afac07f70fd3ddc
kavindulakmal2000 <kavindulakmal2000@gmail.com>
2026-03-19 02:46:29 +0530

    fix logo link#3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -45,7 +45,7 @@ export function Footer() {
           <motion.div variants={itemVariants} className="md:col-span-2 ">
             <div className="flex items-center space-x-2">
             <img
-              src="/gym-promotional-site/logo/logo.png"     
+              src="/logo/logo.png"     
               alt="Logo"
               className="h-20 w-20 object-contain"
             />
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -43,7 +43,7 @@ export function Navigation() {
           >
             {/* Logo Image */}
             <img
-              src="/gym-promotional-site/logo/logo.png"     
+              src="/logo/logo.png"     
               alt="Logo"
               className="h-20 w-20 object-contain"
             />
